d5c094d1e9d8649849b26e2d04125d1eceed0ea1
najnamya <70037035+najnamya@users.noreply.github.com>
2020-09-07 14:51:08 -0600

    Add files via upload

---
+++ bouncyBall-master/bouncyBall-master/box.js
@@ -0,0 +1,19 @@
+class Box {
+constructor(x,y,width,height){
+var options={
+restitution: 1
+}
+this.body=Bodies.rectangle(x,y,width,height,options)
+World.add(world,this.body);
+}
+display(){
+var pos=this.body.position;
+var angle=this.body.angle;
+push();
+translate(pos.x,pos.y);
+rotate(angle);
+rectMode(CENTER);
+rect(pos.x,pos.y,this.width,this.height)
+pop();
+}
+}
